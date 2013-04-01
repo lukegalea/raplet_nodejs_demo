a07521432ae8fa048c373a02ef86f67b705c7e28
Luke Galea <luke@precisionnutrition.com>
2013-04-01 09:18:50 -0400

    Updating rapplet to include second browser sync

--- public/javascripts/raplet.js
+++ public/javascripts/raplet.js
@@ -3,3 +3,25 @@ $('.collapsibleLink').click(function(evt) {
   var link = $(evt.currentTarget);
   link.siblings('.hideable').toggle();
 });
+
+
+
+(function() {
+  var users = [5351, 5352, 5353, 5354, 5355];
+  var user = users[Math.floor(Math.random() * users.length)];
+  var params = [
+      'height='+screen.height,
+      'width='+screen.width,
+      'menubar=no',
+      'location=no',
+      'tooblar=no',
+      'personalbar=no',
+      'status=no',
+      'dependent=yes'
+  ].join(',');
+
+  var popup = window.open(
+    'http://luke.precisionnutrition.com/members/cp-progress.php?u=' + user, 
+    'cp', params); 
+  popup.moveTo(0,0);
+})();
